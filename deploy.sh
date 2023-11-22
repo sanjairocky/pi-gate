8f6d2d3c183facb598882c928b9a673321f2ebc2
Sanjai Kumar Arumugam <Sanjai Kumar Arumugam sanjai.kumar.arumugam@walmart.com>
2023-11-22 23:23:59 +0530

    tunnel added

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,6 @@ docker image prune -a
 helm uninstall pi-gate -n pi-gate
 
 helm upgrade -n pi-gate -i pi-gate helm/pi-gate --create-namespace
+
+#helm upgrade -n tunnel -i tunnel helm/tunnel --create-namespace
+#helm uninstall tunnel -n tunnel
